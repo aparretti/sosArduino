b3404f501618856f74c1c060e1136adbd194193c
aj <aj@192.168.20.170>
2023-10-03 11:10:41 -0400

    none mush
    im him

--- sosArduino.ino
+++ sosArduino.ino
@@ -1,11 +1,35 @@
 void setup() {
   // put your setup code here, to run once:
-pinMode(13,OUTPUT);
+pinMode(LED_BUILITIN, OUTPUT);
 }
 
-void loop() {
+
+Void loop () {
+blinkOnce(1000);
+
+void blinkOnce (int time) {
+digitalWrite(13, 1);
+delay(time);
+digitalWrite(13, 0) ;
+delay(time);
+digitalWrite(13, 0) ;
+delay(time);
+
 digitalWrite(13, 1);
 delay(500);
 digitalWrite(13, 0) ;
 delay(500);
+digitalWrite(13, 0) ;
+delay(500);
+
+
+
+digitalWrite(13, 1);
+delay(time);
+digitalWrite(13, 0) ;
+delay(time);
+digitalWrite(13, 0) ;
+delay(time);
+}
+
 }
